d9181ed6871a539542796e5aad22ae8f1004963b
Nasser Mohamed <nassermohamedit@gmail.com>
2024-05-01 20:00:03 +0100

    Create GuessResult class to hold guess result information

--- src/main/java/nassermohamedit/wordlesolver/Wordle.java
+++ src/main/java/nassermohamedit/wordlesolver/Wordle.java
@@ -4,11 +4,15 @@ import java.util.*;
 
 public class Wordle {
 
+    public static int nextId = 0;
+
     public static final int CORRECT = 1;
 
     public static final int PRESENT = 0;
 
     public static final int ABSENT = -1;
+
+    private final int uid;
 
     private final Config config;
 
@@ -21,6 +25,7 @@ public class Wordle {
     private boolean won;
 
     public Wordle(Config config, String word) {
+        this.uid = nextId++;
         if (config.length != word.length()) {
             throw new IllegalArgumentException();
         }
@@ -32,7 +37,7 @@ public class Wordle {
         }
     }
 
-    public int[] guess(String guessed) {
+    public GuessResult guess(String guessed) {
         if (triesLeft == 0 || won) {
             throw new IllegalStateException("The Game is finished");
         }
@@ -43,38 +48,38 @@ public class Wordle {
             throw new IllegalArgumentException("Not a valid guess");
         }
         --this.triesLeft;
-        int[] feedback = new int[config.length];
+        int[] result = new int[config.length];
         for (int i = 0; i < config.length; ++i) {
             if (charCount[guessed.charAt(i) - 'a'] == 0) {
-                feedback[i] = ABSENT;
+                result[i] = ABSENT;
             }
             else if (guessed.charAt(i) == word.charAt(i)) {
-                feedback[i] = CORRECT;
+                result[i] = CORRECT;
             }
             else {
-                feedback[i] = PRESENT;
+                result[i] = PRESENT;
             }
         }
         for (int i = 0; i < config.length; ++i) {
-            if (feedback[i] == PRESENT) {
+            if (result[i] == PRESENT) {
                 int count = charCount[guessed.charAt(i) - 'a'];
                 for (int j = 0; j < config.length; ++j) {
-                    if (feedback[j] == CORRECT && guessed.charAt(j) == guessed.charAt(i)) {
+                    if (result[j] == CORRECT && guessed.charAt(j) == guessed.charAt(i)) {
                         --count;
                     }
                 }
                 for (int j = 0; j < i && count > 0; ++j) {
-                    if (feedback[j] == PRESENT && guessed.charAt(j) == guessed.charAt(i)) {
+                    if (result[j] == PRESENT && guessed.charAt(j) == guessed.charAt(i)) {
                         --count;
                     }
                 }
                 if (count <= 0) {
-                    feedback[i] = ABSENT;
+                    result[i] = ABSENT;
                 }
             }
         }
-        checkResult(feedback);
-        return feedback;
+        checkResult(result);
+        return new GuessResult(guessed, result, uid);
     }
 
     private void checkResult(int[] feedback) {
@@ -93,6 +98,66 @@ public class Wordle {
     public Config config() {
         return config;
     }
+
+    public int uid() {
+        return uid;
+    }
+
+    public static class GuessResult {
+
+        private final int wordleId;
+
+        private final String word;
+
+        private final List<Integer> result;
+
+        private GuessResult(String word, int[] result, int wordleId) {
+            this.wordleId = wordleId;
+            this.word = word;
+            this.result = Arrays.stream(result).boxed().toList();
+        }
+
+        public String word() {
+            return word;
+        }
+
+        public List<Integer> result() {
+            return result;
+        }
+
+        public int wordleId() {
+            return wordleId;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (obj == this) {
+                return true;
+            }
+            if (!(obj instanceof GuessResult other)) {
+                return false;
+            }
+            if (other.wordleId != wordleId) {
+                return false;
+            }
+            if (!other.word().equals(word)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 5;
+            hash = 89*hash + this.word.hashCode();
+            hash = 89*hash + this.wordleId;
+            return hash;
+        }
+    }
+
 
     public static final class Config {
 
@@ -113,4 +178,3 @@ public class Wordle {
         }
     }
 }
-
--- src/test/java/nassermohamedit/wordlesolver/WordleTest.java
+++ src/test/java/nassermohamedit/wordlesolver/WordleTest.java
@@ -7,17 +7,21 @@ import org.junit.jupiter.api.Test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static nassermohamedit.wordlesolver.Wordle.*;
 
 class WordleTest {
 
-    private static Wordle.Config CONFIG;
+    private static Wordle.Config config;
+
+    private Wordle wordle;
 
     @BeforeAll
-    static void initializeWordle() {
+    static void initializeWordleConfig() {
         Set<String> validGuesses = new HashSet<>();
         String filePath = "wordle-data/allowed_words.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
@@ -25,13 +29,13 @@ class WordleTest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        CONFIG = new Wordle.Config(5, 6, validGuesses);
+        config = new Wordle.Config(5, 6, validGuesses);
     }
 
 
     @Test
     void notAValidGuess_ShouldThrowIllegalArgumentException() {
-        Wordle wordle = new Wordle(CONFIG, "honey");
+        Wordle wordle = new Wordle(config, "honey");
         Assertions.assertThrows(
                 IllegalArgumentException.class, () -> wordle.guess("zzzzz")
         );
@@ -39,7 +43,7 @@ class WordleTest {
 
     @Test
     void notOfValidLength_ShouldThrowIllegalArgumentException() {
-        Wordle wordle = new Wordle(CONFIG, "honey");
+        Wordle wordle = new Wordle(config, "honey");
         Assertions.assertThrows(
                 IllegalArgumentException.class, () -> wordle.guess("strawberry")
         );
@@ -47,57 +51,49 @@ class WordleTest {
 
     @Test
     void allAbsent_ShouldReturnArrayOfAbsents() {
-        Wordle wordle = new Wordle(CONFIG, "honey");
-        int[] actual = wordle.guess("judas");
-        int[] expected = new int[] { ABSENT, ABSENT, ABSENT, ABSENT, ABSENT };
-        Assertions.assertArrayEquals(expected, actual);
+        Wordle wordle = new Wordle(config, "honey");
+        List<Integer> actual = wordle.guess("judas").result();
+        List<Integer> expected = Arrays.asList(ABSENT, ABSENT, ABSENT, ABSENT, ABSENT);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void allPresent_ShouldReturnArrayOfPresents() {
-        Wordle wordle = new Wordle(CONFIG, "honey");
-        int[] actual = wordle.guess("yehno");
-        int[] expected = new int[] { PRESENT, PRESENT, PRESENT, PRESENT, PRESENT };
-        Assertions.assertArrayEquals(expected, actual);
+        Wordle wordle = new Wordle(config, "honey");
+        List<Integer> actual = wordle.guess("yehno").result();
+        List<Integer> expected = Arrays.asList(PRESENT, PRESENT, PRESENT, PRESENT, PRESENT);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void allCorrect_ShouldReturnArrayOfCorrects() {
-        Wordle wordle = new Wordle(CONFIG, "honey");
-        int[] actual = wordle.guess("honey");
-        int[] expected = new int[] { CORRECT, CORRECT, CORRECT, CORRECT, CORRECT };
-        Assertions.assertArrayEquals(expected, actual);
+        Wordle wordle = new Wordle(config, "honey");
+        List<Integer> actual = wordle.guess("honey").result();
+        List<Integer> expected = Arrays.asList(CORRECT, CORRECT, CORRECT, CORRECT, CORRECT);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void randomGuessTest1() {
-        Wordle wordle = new Wordle(CONFIG, "sweet");
-        int[] actual = wordle.guess("tweet");
-        int[] expected = new int[] { ABSENT, CORRECT, CORRECT, CORRECT, CORRECT };
-        Assertions.assertArrayEquals(expected, actual);
+        Wordle wordle = new Wordle(config, "sweet");
+        List<Integer> actual = wordle.guess("tweet").result();
+        List<Integer> expected = Arrays.asList(ABSENT, CORRECT, CORRECT, CORRECT, CORRECT);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void randomGuessTest2() {
-        Wordle wordle = new Wordle(CONFIG, "alien");
-        int[] actual = wordle.guess("annoy");
-        int[] expected = new int[] { CORRECT, PRESENT, ABSENT, ABSENT, ABSENT };
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void randomGuessTest3() {
-        Wordle wordle = new Wordle(CONFIG, "alien");
-        int[] actual = wordle.guess("annoy");
-        int[] expected = new int[] { CORRECT, PRESENT, ABSENT, ABSENT, ABSENT };
-        Assertions.assertArrayEquals(expected, actual);
+        Wordle wordle = new Wordle(config, "alien");
+        List<Integer> actual = wordle.guess("annoy").result();
+        List<Integer> expected = Arrays.asList(CORRECT, PRESENT, ABSENT, ABSENT, ABSENT);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void randomGuessTest4() {
-        Wordle wordle = new Wordle(CONFIG, "nerdy");
-        int[] actual = wordle.guess("nanny");
-        int[] expected = new int[] { CORRECT, ABSENT, ABSENT, ABSENT, CORRECT };
-        Assertions.assertArrayEquals(expected, actual);
+        Wordle wordle = new Wordle(config, "nerdy");
+        List<Integer> actual = wordle.guess("nanny").result();
+        List<Integer> expected = Arrays.asList(CORRECT, ABSENT, ABSENT, ABSENT, CORRECT);
+        Assertions.assertEquals(expected, actual);
     }
 }
