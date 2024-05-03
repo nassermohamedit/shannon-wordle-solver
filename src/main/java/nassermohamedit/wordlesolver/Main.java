01506925cae3c4b2812c2feab71595626abcb8e5
Nasser Mohamed <nassermohamedit@gmail.com>
2024-05-03 16:40:07 +0100

    Driver program

--- src/main/java/nassermohamedit/wordlesolver/Main.java
+++ src/main/java/nassermohamedit/wordlesolver/Main.java
@@ -12,14 +12,14 @@ public class Main {
         Set<String> validGuesses = new HashSet<>();
         String filePath = "wordle-data/allowed_words.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            br.lines().forEach(validGuesses::add);
+            br.lines().filter(l -> l.length() == 5).forEach(validGuesses::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Wordle.Config config = new Wordle.Config(5, 6, validGuesses);
-        Wordle wordle = new Wordle(config, "apple");
-        Guesser randomGuesser = new RandomGuesser(config, wordle.uid());
-        Solver solver = new Solver(wordle, randomGuesser);
+        Wordle.Config config = new Wordle.Config(5, 10, validGuesses);
+        Wordle wordle = new Wordle(config, "bison");
+        Guesser guesser = new ShannonGuesser(config, wordle.uid());
+        Solver solver = new Solver(wordle, guesser);
         solver.solve();
     }
 }
